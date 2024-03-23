9747c44dba925f3973b1bd275886edf233f3f96d
jakareya76 <jakareya1306@gmail.com>
2024-03-23 14:04:36 +0600

    create banner section

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,9 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
-      <header className="container mx-auto">
-        <Navbar />
-      </header>
-
-      <main className="container mx-auto px-5">
-        <Outlet />
-      </main>
-
-      <footer className="py-10 bg-base-200 text-base-content">
-        <Footer />
-      </footer>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
   );
 };
